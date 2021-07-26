9a98801b1d9d054c5adcd767141ef951a3824cfa
WhiteBlackGoose <wbg@angouri.org>
2021-07-26 23:02:22 +0300

    Fixed a func

--- TicTacToeFs/StateTable.fs
+++ TicTacToeFs/StateTable.fs
@@ -15,11 +15,17 @@ let stateTableWith (table : StateTable) x y state =
 
 let emptyStateTable = { States = Array2D.create N N State.Empty; }
 
+type WinRowShape =
+    { StepX : int;
+      StepY : int;
+      RangeX : seq<int>;
+      RangeY : seq<int>; }
+
 let steps = [
-        struct {| StepX = 1; StepY = 0; RangeX = seq { 0..(N - WinRow) }; RangeY = seq { 0..NLow } |};
-        struct {| StepX = 0; StepY = 1; RangeX = seq { 0..NLow }; RangeY = seq { 0..(N - WinRow) } |};
-        struct {| StepX = 1; StepY = 1; RangeX = seq { 0..(N - WinRow) }; RangeY = seq { 0..(N - WinRow) } |};
-        struct {| StepX = 1; StepY = -1; RangeX = seq { 0..(N - WinRow) }; RangeY = seq { WinRow..NLow } |};
+        { StepX = 1; StepY = 0; RangeX = seq { 0..(N - WinRow) }; RangeY = seq { 0..NLow } };
+        { StepX = 0; StepY = 1; RangeX = seq { 0..NLow }; RangeY = seq { 0..(N - WinRow) } };
+        { StepX = 1; StepY = 1; RangeX = seq { 0..(N - WinRow) }; RangeY = seq { 0..(N - WinRow) } };
+        { StepX = 1; StepY = -1; RangeX = seq { 0..(N - WinRow) }; RangeY = seq { WinRow..NLow } };
     ]
 
 let won (table : StateTable) state =
@@ -27,16 +33,16 @@ let won (table : StateTable) state =
         if i = WinRow then
             true
         else
-            if table.States[x, y] == state then
+            if table.States.[x, y] = state then
                 false
             else
                 isBeginOfWinRow (i + 1) (x + stepX) (y + stepY) stepX stepY
 
-    let 
+    let thisShapedWinRowExists stepX stepY rangeX rangeY =
+        Seq.exists (fun (x, y) -> isBeginOfWinRow 0 x y stepX stepY) (Seq.allPairs rangeX rangeY)
 
-    for (stepX, stepY, rangeX, rangeY) in steps do
-        for x in xRange do
-            for y in yRange do
-                if isBeginOfWinRow 0 x y stepX stepY then
-                    
+    steps |>
+    Seq.exists 
+        (fun step -> thisShapedWinRowExists step.StepX step.StepY step.RangeX step.RangeY)
+
 
